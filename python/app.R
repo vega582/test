bfe602c9e8cfe69891734f059b373966004dbae4
vega582 <vedhavicky55@gmail.com>
2023-09-01 12:18:05 +0200

    add rprofile to deployment

--- python/app.R
+++ python/app.R
@@ -9,34 +9,10 @@ library(ggpmisc)
 library(sparkline)
 
 options(shiny.useragg = FALSE)
-VIRTUALENV_NAME <- "example_env_name"
-
-
-# ------------------------- Settings (Edit local settings to match your system) -------------------------- #
-
-if (Sys.info()[["user"]] == "shiny") {
-  # Running on shinyapps.io
-  Sys.setenv(PYTHON_PATH = "python3")
-  Sys.setenv(VIRTUALENV_NAME = VIRTUALENV_NAME) # Installs into default shiny virtualenvs dir
-  Sys.setenv(RETICULATE_PYTHON = paste0("/home/shiny/.virtualenvs/", VIRTUALENV_NAME, "/bin/python"))
-} else if (Sys.info()[["user"]] == "rstudio-connect") {
-  # Running on remote server
-  Sys.setenv(PYTHON_PATH = "/opt/python/3.7.7/bin/python3")
-  Sys.setenv(VIRTUALENV_NAME = paste0(VIRTUALENV_NAME, "/")) # include '/' => installs into rstudio-connect/apps/
-  Sys.setenv(RETICULATE_PYTHON = paste0(VIRTUALENV_NAME, "/bin/python"))
-} else {
-  # Running locally
-  options(shiny.port = 7450)
-  Sys.setenv(PYTHON_PATH = "python3")
-  Sys.setenv(VIRTUALENV_NAME = VIRTUALENV_NAME) # exclude '/' => installs into ~/.virtualenvs/
-  # RETICULATE_PYTHON is not required locally, RStudio infers it based on the ~/.virtualenvs path
-}
-
 
 python_dependencies <- c("pip", "numpy", "pandas")
 virtualenv_dir <- Sys.getenv("VIRTUALENV_NAME")
 python_path <- Sys.getenv("PYTHON_PATH")
-
 reticulate::virtualenv_create(envname = virtualenv_dir, python = python_path)
 reticulate::virtualenv_install(virtualenv_dir, packages = python_dependencies, ignore_installed = TRUE)
 reticulate::use_virtualenv(virtualenv_dir, required = TRUE)
